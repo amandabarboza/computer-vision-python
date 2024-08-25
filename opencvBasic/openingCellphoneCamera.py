789d3a49d232d6b72208a075a0a301f9d1853748
Amanda Barboza <amandab.frc@gmail.com>
2024-08-24 23:41:11 -0300

    fix: comments

--- opencvBasic/openingCellphoneCamera.py
+++ opencvBasic/openingCellphoneCamera.py
@@ -2,10 +2,10 @@ import cv2
 
 video = cv2.VideoCapture()
 
-#instale o app 'IP WebCam' na PlayStore
-#clique em start server
-#copie o ip q possui 'https'
-#insira o ip no parametro abaixo
+# instale o app 'IP WebCam' na PlayStore
+# clique em start server
+# copie o ip q possui 'https'
+# insira o ip no parametro abaixo
 
 ip = "{seuip}/video"
 video.open(ip)
